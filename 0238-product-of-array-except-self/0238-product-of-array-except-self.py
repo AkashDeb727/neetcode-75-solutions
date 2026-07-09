120770411232e291f9c148b4e1f9432b6c227217
AkashDeb727 <84762200+AkashDeb727@users.noreply.github.com>
2026-07-10 02:09:44 +0530

    Time: 23 ms (56.01%), Space: 25.5 MB (55.16%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,38 @@
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+'''
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prefix = [1] * n
+        suffix = [1] * n
+        res = []
+
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+
+        for j in range(n - 2, -1, -1):
+            suffix[j] = suffix[j + 1] * nums[j + 1]
+
+        for i in range(n):
+            res.append(prefix[i] * suffix[i])
+
+        return res
+'''
+
+
+# Space Complexity: O(1)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [1] * n
+        suffix_product = 1
+
+        for i in range(1,n):
+            res[i] *= res[i-1] * nums[i-1]
+        
+        for j in range(n - 1, -1, -1):
+            res[j] *= suffix_product
+            suffix_product *= nums[j]
+
+        return res
