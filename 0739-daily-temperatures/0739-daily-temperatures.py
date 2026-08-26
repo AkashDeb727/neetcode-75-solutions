4a915b0882c1e7b0e2a7737a046d054b28369250
AkashDeb727 <84762200+AkashDeb727@users.noreply.github.com>
2026-08-26 17:34:47 +0530

    Time: 131 ms (12.71%), Space: 35.9 MB (8.27%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,44 @@
+#Brute Force O(n2)
+'''
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = []
+        n = len(temperatures)
+
+        for i in range(n):
+            found = False
+
+            for j in range(i + 1, n):
+                if temperatures[i] < temperatures[j]:
+                    answer.append(j - i)
+                    found = True
+                    break
+
+            if not found:
+                answer.append(0)
+
+        return answer
+'''        
+
+# Monotonic Stack - O(n)
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+
+        # Stores [temperature, index] for unresolved days.
+        # Temperatures in the stack stay monotonically decreasing.
+        stack = []
+
+        for index, temp in enumerate(temperatures):
+
+            # Current temperature resolves previous colder days.
+            while stack and temp > stack[-1][0]:
+                stackTemp, stackIndex = stack.pop()
+
+                # Number of days between the two temperatures.
+                res[stackIndex] = index - stackIndex
+
+            # Current day is now waiting for a warmer future day.
+            stack.append([temp, index])
+        
+        return res
